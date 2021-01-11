1cd58a556782a7d3f66b6a6dd84c593e9128cc1e
jaimecuellar14 <jamescuellar14@gmail.com>
2021-01-11 15:05:21 +0100

    Primeras rutas

---
+++ src/Controller/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/createUser")
+     */
+    public function saveUser(){
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = new Users();
+        $user->setName('Sebas');
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new Response("Se creo un usuario");
+    }
+    
+    /**
+     * @Route("/getUsers")
+     */
+
+     public function getUsers(){
+         $conn= $this->getDoctrine()->getManager()->getConnection();
+
+         $query="SELECT * FROM users";
+         $smtp=$conn->prepare($query);
+         $smtp->execute();
+
+         $response = $smtp->fetchAll();
+         return new Response(json_encode($response));
+     }
+}
---
+++ src/Entity/Users.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UsersRepository::class)
+ */
+class Users
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $nombre;
+
+    public function getName(){
+        return $this->nombre;
+    }
+
+    public function setName($nombre){
+        $this->nombre=$nombre;
+    }
+}
